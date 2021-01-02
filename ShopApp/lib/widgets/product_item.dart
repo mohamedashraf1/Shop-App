1500f0bf2786b5c72ccb9b1b950dece12545e961
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-01-02 02:41:23 +0200

    update product item to render only the favorite icon not the whole widget

--- ShopApp/lib/widgets/product_item.dart
+++ ShopApp/lib/widgets/product_item.dart
@@ -7,7 +7,7 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -24,14 +24,20 @@ class ProductItem extends StatelessWidget {
           ),
         ),
         footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            // the chid is used if you want a specific part of your tree
+            // not to render when update happens... Ex:
+            // label: child
+            // child: Text("this is fixed label")
+            builder: (context, product, child) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
             ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).accentColor,
           ),
           backgroundColor: Colors.black87,
           title: Text(
