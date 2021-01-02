57e0da5fc9b3f5b4ad261eedc05d7cc9ebfaa08d
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-01-02 02:04:44 +0200

    used another provider to path my data to product_item

--- ShopApp/lib/widgets/products_grid.dart
+++ ShopApp/lib/widgets/products_grid.dart
@@ -20,10 +20,9 @@ class ProductsGrid extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (context) => products[i],
+        child: ProductItem(),
       ),
     );
   }
