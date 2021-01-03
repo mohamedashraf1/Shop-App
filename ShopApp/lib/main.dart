1ca6df2d64e5cff84649459b1394ba233e9323d8
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-01-03 03:45:02 +0200

    created an order class to manage my orders

--- ShopApp/lib/main.dart
+++ ShopApp/lib/main.dart
@@ -6,6 +6,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
         )
       ],
       child: MaterialApp(
