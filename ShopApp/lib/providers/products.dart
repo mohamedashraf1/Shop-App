b053f15d98910610992c7bbc8b6617c5d9377708
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-01-25 13:45:04 +0200

    adding the new product to the list of products

--- ShopApp/lib/providers/products.dart
+++ ShopApp/lib/providers/products.dart
@@ -65,8 +65,15 @@ class Products with ChangeNotifier {
     return _items.firstWhere((element) => element.id == id);
   }
 
-  void addProduct() {
-    //_items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
     notifyListeners();
   }
 }
