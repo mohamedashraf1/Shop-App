355a2c926bd4a448ccba1959cbbd5647e530d3da
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-02-02 14:10:23 +0200

    storing the favorite status on server

--- ShopApp/lib/widgets/product_item.dart
+++ ShopApp/lib/widgets/product_item.dart
@@ -8,6 +8,7 @@ import '../providers/cart.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
@@ -35,8 +36,17 @@ class ProductItem extends StatelessWidget {
               icon: Icon(
                 product.isFavorite ? Icons.favorite : Icons.favorite_border,
               ),
-              onPressed: () {
-                product.toggleFavoriteStatus();
+              onPressed: () async {
+                try {
+                  await product.toggleFavoriteStatus(product.id);
+                } catch (error) {
+                  scaffold.showSnackBar(SnackBar(
+                      duration: Duration(milliseconds: 1500),
+                      content: Text(
+                        "Something went wrong!",
+                        textAlign: TextAlign.center,
+                      )));
+                }
               },
               color: Theme.of(context).accentColor,
             ),
