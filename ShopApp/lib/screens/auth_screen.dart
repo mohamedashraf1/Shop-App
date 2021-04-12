f4d8843162ed37702558c12bce1dc77f4e47b009
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-04-12 17:53:43 +0200

    removed unnecessary import

--- ShopApp/lib/screens/auth_screen.dart
+++ ShopApp/lib/screens/auth_screen.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/screens/products_overview_screen.dart';
 
 import '../providers/auth.dart';
 import '../models/http_exception.dart';
