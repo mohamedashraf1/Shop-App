51d7057a4fdc9fc658805d286c39d7662c974d08
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-04-22 01:22:17 +0200

    Automatically Logging Users In

--- ShopApp/lib/main.dart
+++ ShopApp/lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/product_detail_screen.dart';
-import './screens/products_overview_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
-import './screens/cart_screen.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
+
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
-import './providers/auth.dart';
+import './screens/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -53,7 +55,16 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: auth.isAuth ? ProductOverviewScreen() : AuthScreen(),
+          home: auth.isAuth
+              ? ProductOverviewScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
