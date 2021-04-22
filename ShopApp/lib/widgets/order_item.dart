e5e5450446977a979646e747ed62b9f48f609f1c
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-04-22 18:05:30 +0200

    adding animation to the order screen

--- ShopApp/lib/widgets/order_item.dart
+++ ShopApp/lib/widgets/order_item.dart
@@ -36,34 +36,38 @@ class _OrderItemState extends State<OrderItem> {
               },
             ),
           ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
-              child: ListView(
-                children: widget.order.products
-                    .map((prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              prod.title,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+          AnimatedContainer(
+            curve: Curves.easeIn,
+            duration: Duration(milliseconds: 300),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            // height: min(widget.order.products.length * 20.0 + 10, 100),
+            height: _expanded
+                ? min(widget.order.products.length * 20.0 + 10, 100)
+                : 0,
+            child: ListView(
+              children: widget.order.products
+                  .map((prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Text(
-                              "${prod.quantity}x \$${prod.price}",
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ))
-                    .toList(),
-              ),
-            )
+                          ),
+                          Text(
+                            "${prod.quantity}x \$${prod.price}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ))
+                  .toList(),
+            ),
+          ),
         ],
       ),
     );
